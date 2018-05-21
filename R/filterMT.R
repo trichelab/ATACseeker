a0391a10c74c88b0aaf9cf44eea5c589ca3b3e47
ttriche <tim.triche@gmail.com>
2018-05-21 13:52:46 -0400

    fix a bug in filterMT

--- R/filterMT.R
+++ R/filterMT.R
@@ -1,10 +1,10 @@
-#' Filter a RangedSummarizedExperiment's columns based on its $mtCovg mcols 
+#' Filter a RangedSummarizedExperiment's columns based on its $mtCovg colData 
 #'
 #' Griffin et al. (Genetics in Medicine 2014) recommends 20x coverage for mtDNA
 #' sequencing to have comparable error rates to Sanger sequencing.  By default, 
 #' that is the cutoff applied here to ensure halfway decent variant annotation.
 #' 
-#' @param SE          a RangedSummarizedExperiment with an mcols() named mtCovg
+#' @param SE          a RangedSummarizedExperiment with colData() named mtCovg
 #' @param minCovg     minimum covg (20, cf. Griffin, Genetics in Medicine 2014)
 #'
 #' @import SummarizedExperiment
@@ -14,8 +14,8 @@ filterMT <- function(SE, minCovg=20) {
 
   if (!is(SE, "RangedSummarizedExperiment")) {
     stop("filterMT operates on a RangedSummarizedExperiment.")
-  } else if (! "mtCovg" %in% names(mcols(SE)) ) { 
-    stop("filterMT requires a RangedSummarizedExperiment with mcols 'mtCovg'.")
+  } else if (! "mtCovg" %in% names(colData(SE)) ) { 
+    stop("filterMT requires a RangedSummarizedExperiment with colData $mtCovg.")
   }
 
   return(SE[, which(SE$mtCovg >= minCovg)])
