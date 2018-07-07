a7b2307b3eb4a1847979d9742bb8a57b297640fc
Ben Johnson <ben.johnson4343@gmail.com>
2018-07-07 15:42:01 -0500

    compatibility fixes for new preseqR

--- R/getEsts.R
+++ R/getEsts.R
@@ -7,17 +7,13 @@
 #' Original functions in preseqR v2.0.1.1 for this were:
 #' preseqR.rfa.curve and preseqR.rfa.species.accum.curve
 #' 
-#' The new functions as of the version 3.1.2 are:
+#' The new functions as of the version 4.0.0 are:
 #' 
-#' ds.mincount == preseqR.rfa.curve
-#' ds.mincount.bootstrap == preseqR.rfa.species.accum.curve
+#' ds.rSAC == preseqR.rfa.curve
+#' ds.rSAC.bootstrap == preseqR.rfa.species.accum.curve
 #' 
 #' The new functions return generators that can have data passed to them
 #' instead of returning a data frame as in version 2.0.1.1.
-#' 
-#' It is worth noting that these functions return *non-bootstrapped* 
-#' fragment estimates but uses the bootstrapped variance estimates to 
-#' calculate CI. This is a function of the latest implementation of preseqR.
 #'
 #' @param xx      The fragments or sample of fragments
 #' @param withCI  Have preseq compute 95 percent confidence intervals for plots?
@@ -32,23 +28,21 @@ getEsts <- function(xx, withCI=FALSE, ...) {
   if (is(xx, "GRanges") | is(xx, "GAlignmentPairs")) xx <- getComplexity(xx)
   message("Estimating complexity (this can take a little while)...")
   if (!withCI) {
-    res.fun <- ds.mincount(xx, mt = 100, ...)
+    res.fun <- suppressWarnings(ds.rSAC(xx, mt = 100, ...))
     message("Done generating complexity estimator...")
     message("Building library complexity curves...")
     res <- data.frame(reads = as.double(xx[, 1] %*% xx[, 2]) * 1:100,
-                      frags = sapply(1:100, res.fun$FUN))
+                      frags = sapply(1:100, res.fun$f))
   } else {
-    res.fun <- ds.mincount.bootstrap(xx, mt = 100, times = 99, ...)
+    res.fun <- suppressWarnings(ds.rSAC.bootstrap(xx, mt = 100, times = 99, conf = 0.95, ...))
     message("Done generating complexity estimator...")
     message("Building library complexity curves...")
-    message("Calculating 95% CI...")
-    res.ci <- sapply(1:100, function(x) {
-      exp(qnorm((1 + 0.95) / 2.0) * sqrt(log(1.0 + res.fun$var(x) / (res.fun$FUN.nobootstrap$FUN(x)^2))))
-    })
+    res.ci.lb <- sapply(1:100, res.fun$lb)
+    res.ci.ub <- sapply(1:100, res.fun$ub)
     res <- data.frame(reads = as.double(xx[, 1] %*% xx[, 2]) * 1:100,
-                      frags = sapply(1:100, res.fun$FUN.nobootstrap$FUN))
-    res$frags.lower = res$frags / res.ci
-    res$frags.upper = res$frags * res.ci
+                      frags = sapply(1:100, res.fun$f),
+                      lb = res.ci.lb,
+                      ub = res.ci.ub)
   }
   return(res)
 }
