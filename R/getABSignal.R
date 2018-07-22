f23942433e755f54ce595b29a935d818f53e1da2
Ben Johnson <ben.johnson4343@gmail.com>
2018-07-21 23:46:54 -0400

    AB compartment updates

--- R/getABSignal.R
+++ R/getABSignal.R
@@ -3,7 +3,7 @@
 #' This function is used to generate a list x to be passed to getABSignal
 #'
 #' @param x      A list object from getCorMatrix
-#' @param k    Bin size for smoothing (default = 2)
+#' @param k    Value of k for smoothing (default = 2)
 #' @param iter    Number of iterations for moving average smoothing (default = 2)
 #' 
 #' @return    A list x to pass to getABSignal
@@ -16,10 +16,13 @@
 getABSignal <- function(x, k = 2, iter = 2){
   message("Calculating eigenvectors...")
   pc <- .getFirstPC(x$binmat.cor)
-  message(paste0("Smoothing with a bin size of ", k, " and ", iter, " iterations..."))
+  message(paste0("Smoothing with a k of ", k, " for ", iter, " iterations..."))
   pc <- .meanSmoother(pc, k=k, iter=iter)
   message("Done smoothing...")
-  return(list(pc=pc, gr=x$gr))
+  gr <- x$gr
+  gr$pc <- pc
+  gr$compartments <- .extractOpenClosed(pc)
+  return(gr)
 }
 
 #Internal function
@@ -45,26 +48,41 @@ getABSignal <- function(x, k = 2, iter = 2){
 # Author: Jean-Philippe Fortin
 # May 6th 2015
 
-.meanSmoother <- function(x, k = 2L, iter = 2L, na.rm = TRUE) {
+.meanSmoother <- function(x, k=1, iter=2, na.rm=TRUE){
+  meanSmoother.internal <- function(x, k=1, na.rm=TRUE){
     n <- length(x)
-    y <- rep(NA_real_, n)
+    y <- rep(NA,n)
     
-    window.mean <- function(x, j, k, na.rm = na.rm){
-      if (k >= 1) {
-        return(mean(x[seq(j - (k + 1L), j + k)], na.rm = na.rm))
+    window.mean <- function(x, j, k, na.rm=na.rm){
+      if (k>=1){
+        return(mean(x[(j-(k+1)):(j+k)], na.rm=na.rm))
       } else {
-        x[j]
-      }
+        return(x[j])
+      }    
     }
     
-    for (i in (seq(k + 1L, n - k))) {
-      y[i] <- window.mean(x, i, k, na.rm)
+    for (i in (k+1):(n-k)){
+      y[i] <- window.mean(x,i,k, na.rm)
     }
-    for (i in seq_len(k)) {
-      y[i] <- window.mean(x, i, i - 1L, na.rm)
+    for (i in 1:k){
+      y[i] <- window.mean(x,i,i-1, na.rm)
     }
-    for (i in seq(n - k + 1L, n)) {
-      y[i] <- window.mean(x, i, n - i, na.rm)
+    for (i in (n-k+1):n){
+      y[i] <- window.mean(x,i,n-i,na.rm)
     }
     y
+  }
+  
+  for (i in 1:iter){
+    x <- meanSmoother.internal(x, k=k, na.rm=na.rm)
+  }
+  x
+}
+
+#Internal function
+#Code modified from minfi
+# Author: Jean-Philippe Fortin
+# May 6th 2015
+.extractOpenClosed <- function(pc, cutoff = 0){
+  ifelse(pc < cutoff, "open", "closed")
 }
